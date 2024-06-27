4377b17bd51f5486db7bd723b5e8dcbc414077d9
CEJSH <eun2763@naver.com>
2024-06-27 11:50:53 +0900

    csr

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -13,6 +13,8 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
+export const revalidate = 3;
+
 export default async function ProductPage({ params: { slug } }: Props) {
   const product = await getProduct(slug);
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,14 @@
 import { getProducts } from "@/service/products";
+import MeowArticle from "@/components/MeowArticle";
 import Link from "next/link";
 import React from "react";
+
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
+
   return (
     <div className="flex flex-col">
       <h1>제품 소개 페이지!</h1>
@@ -15,6 +19,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </div>
   );
 }
