b06b79f6e0f92f040bff507d5cf94c5e837c8cd7
CEJSH <eun2763@naver.com>
2024-06-28 14:37:53 +0900

    Version3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   console.log(os.hostname());
   return (
     <>
-      <h1>홈페이지다!! 버전2</h1>
+      <h1>홈페이지다!! 버전3</h1>
       <Counter />
       <Image
         src="https://images.unsplash.com/photo-1719165626474-c7cbb9c6416b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
